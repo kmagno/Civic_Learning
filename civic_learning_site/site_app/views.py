48c64aff2a839976da4052e2ed9e11780dddf3cc
Allison King <allisonjuliaking@gmail.com>
2017-04-22 13:52:29 -0400

    add start bootstrap template

--- civic_learning_site/site_app/views.py
+++ civic_learning_site/site_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'site_app/home.html', {})
+    return render(request, 'site_app/index.html', {})
