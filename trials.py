b347dd2f61810d7fa0e7bed10c3da6556d202a13
divyaagarwal <agarwal.d@husky.neu.edu>
2017-04-22 16:20:17 -0400

    Get the links in trials.py

--- trials.py
+++ trials.py
@@ -4,4 +4,11 @@ from pprint import pprint
 with open('MALower.json') as data_file:
     data = json.load(data_file)
 
-pprint(data)
+print(data["Results"][0]["Websites"]["Office"])
+for i in range(len(data["Results"])):
+    x = data["Results"][i]["Websites"]["Office"]
+    print(x)
+    print ("--------------------------------------")
+# for i in range(len(data)):
+#     x =data[i].Websites.Office
+#     pprint(x)
